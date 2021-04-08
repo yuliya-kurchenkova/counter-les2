c5fa9f69dbf85f100b27db400d6d18c08224cbc5
Kurchenkova Yulia <iskra-000@yandex.com>
2021-04-08 21:13:17 +0300

    add problem onChange

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import HW1 from './hw/1-simple';
+import HW2 from './hw/2-input';
+
 
 export default function (){
     return (
         <div>
             <h2>Minmax simple</h2>
             <HW1 min={1} max={5}/>
+            <h2>Minmax input</h2>
+            <HW2 min={20} max={50}/>
         </div>
     )
 }
