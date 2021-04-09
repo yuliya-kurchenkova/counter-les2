4c77e892e027583471c97611ad762341f73206e3
Kurchenkova Yulia <iskra-000@yandex.com>
2021-04-09 12:08:03 +0300

    add decision onChange

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import HW1 from './hw/1-simple';
 import HW2 from './hw/2-input';
+import HW3 from './hw/3-lazy';
 
 
 export default function (){
@@ -10,6 +11,8 @@ export default function (){
             <HW1 min={1} max={5}/>
             <h2>Minmax input</h2>
             <HW2 min={20} max={50}/>
+            <h2>Lazy input</h2>
+            <HW3 min={20} max={50}/>
         </div>
     )
 }
