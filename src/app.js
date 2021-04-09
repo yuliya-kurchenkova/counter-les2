ae491f0caf8aba6340c5e4ac7e61b46abe1c2f08
Kurchenkova Yulia <iskra-000@yandex.com>
2021-04-09 12:34:59 +0300

    add static getDerivedStateFromProps

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import HW1 from './hw/1-simple';
 import HW2 from './hw/2-input';
 import HW3 from './hw/3-lazy';
-
+import Derived from './hw/4-deriveds'
 
 export default function (){
+    let [someMin, setSomeMin] = useState(20)
+
+    setTimeout(() => {
+        setSomeMin(40)
+    }, 2000)
+
     return (
         <div>
             <h2>Minmax simple</h2>
@@ -13,6 +19,8 @@ export default function (){
             <HW2 min={20} max={50}/>
             <h2>Lazy input</h2>
             <HW3 min={20} max={50}/>
+            <h2>Derived input</h2>
+            <Derived min={someMin} max={50}/>
         </div>
     )
 }
